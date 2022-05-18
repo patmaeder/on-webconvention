91b08d585da5937ff44f41af14915b1eafbb55f0
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2022-05-18 12:29:52 +0200

    configure ddev environment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from "nuxt";
+import "dotenv/config";
+import fs from "fs";
+
+const viteConfig = {
+  server: {
+    hmr: {
+      protocol: "wss",
+      host: "localhost",
+    },
+    https: {
+      key: fs.readFileSync(".ddev/ssl/certs/master.key"),
+      cert: fs.readFileSync(".ddev/ssl/certs/master.crt"),
+    },
+  },
+};
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+  server: {},
+  vite: viteConfig,
+});
