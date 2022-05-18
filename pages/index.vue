9bb5c5e99cdc611d5a4faee21961fdcfc2d6ec9c
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2022-05-18 17:32:46 +0200

    example: display users in frontend

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,26 @@
 <template>
   <h1>Nuxt 3 starter template with all folders and hint</h1>
+  <p>Registrierte Benutzer:</p>
+  <ul>
+    <li v-for="user in users">{{user.email}}</li>
+  </ul>
 </template>
 
 <script setup>
+import {useServer} from "../server";
+
+const { data: users } = await useAsyncData('fetch.users', async () => {
+
+  const { prisma } = await useServer();
+
+  return await prisma.user.findMany({
+    select: {
+      email: true
+    }
+  });
+})
+
+console.log(users);
 </script>
 
 <style lang="scss" scoped>
