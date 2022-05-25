2b1b96359c16f6d3a35ff94f2981f4f56bbe11ca
patmaeder <padi.maeder@web.de>
2022-05-26 00:20:46 +0200

    Created EventSiteRenderer Component

---
+++ pages/event-site.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="eventSite">
+        <EventSiteRenderer ref="eventSiteRenderer"/>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+#eventSite {
+    width: 100vw;
+    height: 100vh;
+    background-color: #252830;
+}
+</style>
