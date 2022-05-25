67ec7a58e1ab7fd3f237f97e24978793a1ad7d06
patmaeder <padi.maeder@web.de>
2022-05-26 00:33:31 +0200

    Created basic troisJS scene to render the individual rooms

--- components/EventSiteRenderer.vue
+++ components/EventSiteRenderer.vue
@@ -1,8 +1,67 @@
 <template>
+    <div id="event-site_wrapper" ref="eventSiteWrapper">
+        <client-only>
+            <Renderer ref="renderer" alpha antialias orbitCtrl resize="true" :pointer="{intersectRecursive: true}">
+                <Camera ref="camera" :position="{x: -10, y: 6, z: 8}" />
+                <Scene ref="scene">
+                    <PointLight :position="{x: -8, y: 10, z: 4 }" :intensity="1.4"/>
+                    <GltfModel v-for="tile in eventSite" :key="tile.id" :src="'/glbModels/' + tile.src" :position="tile.position" />                            
+                </Scene>
+            </Renderer>
+        </client-only>
+    </div>
 </template>
 
 <script lang="ts" setup>
+import { Renderer, Camera, Scene, PointLight, GltfModel } from "troisjs";
+
+
+/*
+ * ---------------
+ * Variables
+ * ---------------
+ */
+
+// References to Scene Objects
+const eventSiteWrapper = ref(null);
+const renderer = ref(null);
+const camera = ref(null);
+const scene = ref(null);
+
+
+/*
+ * ---------------
+ * Props
+ * ---------------
+ */
+const props = defineProps({
+    eventSite: Object,
+    user: Object,
+})
+
+
+/*
+ * ---------------
+ * On Mounted Hook
+ * --------------
+ */
+onMounted(() => {
+    nextTick(() => {
+
+        // Adjust default Three.js Orbitcontrol Properties
+        const controls = renderer.value.three.cameraCtrl;
+        controls.enableRotate = false;
+        controls.minDistance = 1;
+        controls.maxDistance = 14;
+        controls.mouseButtons = {LEFT: 2, MIDDLE: 1, RIGHT: 0};
+        controls.screenSpacePanning = false;        
+    })
+})
 </script>
 
 <style>
+#event-site_wrapper {
+    width: 100%;
+    height: 100%;
+}
 </style>
--- pages/event-site.vue
+++ pages/event-site.vue
@@ -1,11 +1,64 @@
 <template>
     <div id="eventSite">
-        <EventSiteRenderer ref="eventSiteRenderer"/>
+        <EventSiteRenderer ref="eventSiteRenderer" :eventSite="eventSite" :user="user" />
     </div>
 </template>
 
 <script lang="ts" setup>
 
+const route = useRoute()
+
+const user = {
+    id: route.query.user,
+    role: "visitor"
+}
+
+const eventSite = {
+   500000: {
+        id: 500000,
+        name: 'Empfangsbereich',
+        type: "lobby",
+        src: "lobby.glb",
+        position: {
+            x: 0,
+            y: 0,
+            z: 0.5
+        }
+   },
+   500001: {
+        id: 500001,
+        name: 'Meetingraum 1',
+        type: "keynote",
+        src: "keynote.glb",
+        position: {
+            x: 1,
+            y: 0,
+            z: 0
+        },
+    }, 
+    500002: {
+        id: 500002,
+        name: 'Meetingraum 2',
+        type: "keynote",
+        src: "keynote.glb",
+        position: {
+            x: 1,
+            y: 0,
+            z: 1
+        },
+    }, 
+    500003: {
+        id: 500003,
+        name: 'Pausenraum',
+        type: "break",
+        src: "break.glb",
+        position: {
+            x: 0,
+            y: 0,
+            z: -1
+        },
+    },
+}
 </script>
 
 <style>
