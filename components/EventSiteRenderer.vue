304aae5d37bbd10acfadfdb7d99da7991b925525
patmaeder <padi.maeder@web.de>
2022-05-29 01:22:54 +0200

    Sync character movement across all clients

--- components/EventSiteRenderer.vue
+++ components/EventSiteRenderer.vue
@@ -12,10 +12,10 @@
                         v-if="showChar" 
                         ref="character" 
                         :src="user.role == 'visitor' ?  '/glbModels/visitor.glb' : '/glbModels/speaker.glb'" 
-                        :position="{x: 0, y: 0.05, z: 0}"
-                        :rotation="{x: 0, y: 0, z: 0}"
+                        :position="usersSharedMap.has(props.user.id) ? {...usersSharedMap.get(props.user.id).position} : {x: 0, y: 0.05, z: 0}"
+                        :rotation="usersSharedMap.has(props.user.id) ? {x: usersSharedMap.get(props.user.id).rotation._x, y: usersSharedMap.get(props.user.id).rotation._y, z: usersSharedMap.get(props.user.id).rotation._z} : {x: 0, y: 0, z: 0}"
                         :scale="{x: 0.4, y: 0.4, z: 0.4}" 
-                        @load="charLoaded"
+                        @load="characterLoaded"
                     />
                 </Scene>
             </Renderer>
@@ -44,6 +44,9 @@
 import { Renderer, Camera, Scene, PointLight, GltfModel } from "troisjs";
 import * as THREE from "three";
 import { CSS2DRenderer, CSS2DObject } from "three/examples/jsm/renderers/CSS2DRenderer.js";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import * as Y from "yjs"
+import { WebsocketProvider } from "y-websocket";
 
 /*
  * ---------------
@@ -60,18 +63,23 @@ const scene = ref(null);
 const character = ref(null);
 let characterObject3D: THREE.Object3D;
 
-// Variables related to character movement
-let clock: THREE.Clock;
-let delta;
-let showChar = ref(false);
-const rotateAngle: THREE.Vector3 = {x: 0, y: 1, z: 0};
-
 // Variables needed for showing popover
 const eventSiteOverlay = ref(null);
 const popover = ref(null);
 let popoverCSS2DObject: CSS2DObject;
 let selectedRoom = ref(null);
 
+// Variables related to character movement
+let clock: THREE.Clock;
+let delta;
+let showChar = ref(false);
+const rotateAngle: THREE.Vector3 = {x: 0, y: 1, z: 0};
+
+// Variables needed for websocket connection
+let wsProvider: WebsocketProvider;
+const doc = new Y.Doc();
+let users = {};
+const usersSharedMap = doc.getMap("users");
 
 /*
  * ---------------
@@ -100,8 +108,8 @@ const emit = defineEmits(["favorEvent"])
  * ---------------
  */
 const currentEvents = computed(() => {
-    let temp = []
-    const time = new Date().getTime()
+    let temp = [];
+    const time = new Date().getTime();
 
     for (let [key, value] of Object.entries(props.events)) {
         value.room == selectedRoom.value && new Date(value.end_date).getTime() > time ? temp.push({id: key, ...value}): null;
@@ -120,11 +128,20 @@ const currentEvents = computed(() => {
  * Functions
  * ---------------
  */
-function charLoaded(gltf) {
+function characterLoaded(gltf) {
     characterObject3D = gltf.scene;
 
     document.addEventListener("keydown", keyDown);
     document.addEventListener("keyup", keyUp);
+
+    // Add user to sharedMap
+    if (!usersSharedMap.has(props.user.id)) {
+        usersSharedMap.set(props.user.id, {
+            role: props.user.role,
+            position: {x: 0, y: 0.05, z: 0},
+            rotation: {_x: 0, _y: 0, _z: 0},
+        })
+    }
 
     // Callback that is executed before every rerender to move characters
     renderer.value.onBeforeRender(() => {
@@ -137,10 +154,53 @@ function charLoaded(gltf) {
                     character.value.userData.move.speed += (Math.log(character.value.userData.move.speed) * -1 + 2.5) * 0.1;
                 }
 
-                moveChar(characterObject3D, character.value.userData.move.forward, character.value.userData.move.sideward, character.value.userData.move.speed, delta);
+                moveCharacter(characterObject3D, character.value.userData.move.forward, character.value.userData.move.sideward, character.value.userData.move.speed, delta);
+
+                usersSharedMap.set(props.user.id, {
+                    role: props.user.role,
+                    position: characterObject3D.position,
+                    rotation: characterObject3D.rotation
+                })
             }
         }
     })
+}
+
+function moveCharacter(model, forward, sideward, speed) {
+
+    let rotateQuaternion: THREE.Quaternion = new THREE.Quaternion();
+    let angle = 0;
+
+    switch(true) {
+        case (forward == -1 && sideward == -1):
+            angle = 0.25 * Math.PI;
+            break;
+        case (forward == -1 && sideward == 1):
+            angle = -0.25 * Math.PI;
+            break;
+        case (forward == -1):
+            angle = 0;
+            break;
+        case (forward == 1 && sideward == -1):
+            angle = 0.75 * Math.PI;
+            break;
+        case (forward == 1 && sideward == 1): 
+            angle = -0.75 * Math.PI;
+            break;
+        case (forward == 1):
+            angle = Math.PI;
+            break;
+        case (sideward == -1):
+            angle = 0.5 * Math.PI;
+            break;
+        case (sideward == 1):
+            angle = -0.5 * Math.PI;
+            break;
+    }
+
+    rotateQuaternion.setFromAxisAngle(rotateAngle, angle);
+    model.quaternion.rotateTowards(rotateQuaternion, 0.04);
+    model.translateX(0.02 * delta * speed);
 }
 
 function keyDown(evt) {
@@ -149,25 +209,25 @@ function keyDown(evt) {
     let sideward = (character.value.userData.move !== undefined) ?  character.value.userData.move.sideward : 0;
     
     switch(evt.keyCode){
-        case 87://W
-        case 38://Arrow up
+        case 87: //W
+        case 38: //Arrow up
             forward = -1;
-            updateCharacterUserData(forward, sideward);
+            setDirectionOfMovement(forward, sideward);
             break;
-        case 83://S
-        case 40://Arrow down
+        case 83: //S
+        case 40: //Arrow down
             forward = 1;
-            updateCharacterUserData(forward, sideward);
+            setDirectionOfMovement(forward, sideward);
             break;
-        case 65://A
-        case 37://Arrow left
+        case 65: //A
+        case 37: //Arrow left
             sideward = -1;
-            updateCharacterUserData(forward, sideward);
+            setDirectionOfMovement(forward, sideward);
             break;
-        case 68://D
-        case 39://Arrow right
+        case 68: //D
+        case 39: //Arrow right
             sideward = 1;
-            updateCharacterUserData(forward, sideward);
+            setDirectionOfMovement(forward, sideward);
             break;
     }
 }
@@ -177,24 +237,24 @@ function keyUp(evt){
     let sideward = (character.value.userData.move !== undefined) ?  character.value.userData.move.sideward : 0;
     
     switch(evt.keyCode){
-      case 87://W
-      case 38://Arrow up
-      case 83://S
-      case 40://Arrow down
+      case 87: //W
+      case 38: //Arrow up
+      case 83: //S
+      case 40: //Arrow down
         forward = 0;
-        updateCharacterUserData(forward, sideward);
+        setDirectionOfMovement(forward, sideward);
         break;
-      case 65://A
-      case 37://Arrow left
-      case 68://D
-      case 39://Arrow right
+      case 65: //A
+      case 37: //Arrow left
+      case 68: //D
+      case 39: //Arrow right
         sideward = 0;
-        updateCharacterUserData(forward, sideward);
+        setDirectionOfMovement(forward, sideward);
         break;
     }
 }
 
-function updateCharacterUserData(forward, sideward){
+function setDirectionOfMovement(forward, sideward){
 
     if (character.value.userData.move && (forward != 0 || sideward != 0)){
         character.value.userData.move.forward = forward;
@@ -205,34 +265,25 @@ function updateCharacterUserData(forward, sideward){
     }
 }
 
-function moveChar(model, forward, sideward, speed) {
+function addUser(key, user) {
+    const loader = new GLTFLoader();
+    const src = user.role == "visitor" ? "/glbModels/visitor.glb" : "/glbModels/speaker.glb";
 
-    let rotateQuaternion: THREE.Quaternion = new THREE.Quaternion();
+    loader.load(
+        src,
+        function (gltf) {
+            scene.value.add(gltf.scene);
 
-    if (forward == -1) {
-        if (sideward == -1) {
-            rotateQuaternion.setFromAxisAngle(rotateAngle, 0.25 * Math.PI); // w+a
-        } else if (sideward == 1) {
-            rotateQuaternion.setFromAxisAngle(rotateAngle, -0.25 * Math.PI); // w+d
-        } else {
-            rotateQuaternion.setFromAxisAngle(rotateAngle, 0); //w
-        }
-    } else if (forward == 1) {
-        if (sideward == -1) {
-            rotateQuaternion.setFromAxisAngle(rotateAngle, 0.75 * Math.PI); // s+a
-        } else if (sideward == 1) {
-            rotateQuaternion.setFromAxisAngle(rotateAngle, -0.75 * Math.PI); // s+d
-        } else {
-            rotateQuaternion.setFromAxisAngle(rotateAngle, Math.PI); // s
-        }
-    } else if (sideward == -1) {
-        rotateQuaternion.setFromAxisAngle(rotateAngle, 0.5 * Math.PI); // a
-    } else if (sideward == 1) {
-        rotateQuaternion.setFromAxisAngle(rotateAngle, -0.5 * Math.PI); // d
-    }
-
-    model.quaternion.rotateTowards(rotateQuaternion, 0.04);
-    model.translateX(0.02 * delta * speed);
+            gltf.scene.position.set(user.position.x, user.position.y, user.position.z);
+            gltf.scene.scale.set(0.4, 0.4, 0.4)
+            gltf.scene.rotation.set(user.rotation._x, user.rotation._y, user.rotation._z)
+            
+            users[key] = {
+                model: gltf.scene,
+                ...user
+            }
+        },
+    );
 }
 
 function focusRoom(roomID, event) {
@@ -270,11 +321,6 @@ onMounted(() => {
         controls.mouseButtons = {LEFT: 2, MIDDLE: 1, RIGHT: 0};
         controls.screenSpacePanning = false;
 
-        // EventListener to render character on user input
-        document.addEventListener("keydown", function listenForCharInput (event) {
-            [87, 38, 83, 40, 65, 37, 68, 39].includes(event.keyCode) ? showChar.value = true : null;
-        }, {once: true})
-
         // Setup CSS2DObject and CSS2DRenderer
         popoverCSS2DObject = new CSS2DObject(popover.value)
         labelRenderer = new CSS2DRenderer({element: eventSiteOverlay.value});
@@ -284,10 +330,57 @@ onMounted(() => {
             labelRenderer.setSize(eventSiteWrapper.value.clientWidth, eventSiteWrapper.value.clientHeight);
         }))
 
+        // EventListener to render character on user input
+        document.addEventListener("keydown", function listenForCharInput (event) {
+            [87, 38, 83, 40, 65, 37, 68, 39].includes(event.keyCode) ? showChar.value = true : null;
+        }, {once: true})
+
+        // Establish Websocket connection and listen for changes
+        wsProvider = new WebsocketProvider("ws://"+ window.location.hostname +":1234", "event-site", doc);
+
+        usersSharedMap.observe(event => {
+            for(let [key, value] of event.changes.keys) {
+
+                switch (value.action) {
+
+                    case "add":
+                        if (key != props.user.id) {
+                            addUser(key, usersSharedMap.get(key));
+
+                        } else if (!showChar.value) {
+                            showChar.value = true;
+                        }
+                        break;
+                    
+                    case "update":
+                        if (key != props.user.id) {
+                            users[key] = {
+                                ...users[key],
+                                ...usersSharedMap.get(key)
+                            }
+                        }
+                        break;
+
+                    case "delete":
+                        if (key != props.user.id) {
+                            scene.value.remove(users[key].character);
+                            delete users[key];
+                        }
+                        break;
+                }
+            }
+        })
+
         renderer.value.onBeforeRender(() => {
             delta = clock.getDelta();
             labelRenderer.render(renderer.value.scene, renderer.value.camera);
             popoverCSS2DObject.position.set(0, (renderer.value.three.cameraCtrl.getDistance() * 0.048 + 0.12), 0);
+        
+            for (let [key, user] of Object.entries(users)) {
+
+                user.model.rotation.set(user.rotation._x,user.rotation._y, user.rotation._z);
+                user.model.position.lerp(user.position, 0.5)
+            }
         })
     })
 })
@@ -354,7 +447,7 @@ onMounted(() => {
     justify-items: center;
 }
 
-#event-site_overlay .favorite{
+#event-site_overlay .favorite {
     color: #79F0DA;
 }
 </style>
