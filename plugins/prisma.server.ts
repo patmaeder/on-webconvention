e0f6f189730708f1d59d1bea46f5ff4e49119a1b
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2022-05-29 00:44:19 +0200

    refactor file to use .server suffix

--- plugins/prisma.server.ts
+++ plugins/prisma.server.ts
@@ -2,10 +2,6 @@ import { PrismaClient } from "@prisma/client";
 import { defineNuxtPlugin } from "#app";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  if (!process.server) {
-    return;
-  }
-
   const prisma = new PrismaClient();
 
   // bind prisma client to nuxt via $prisma
