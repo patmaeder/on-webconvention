4a0918219b55c42d26828cc28c44cc037b0944a6
MartinPanaget <m.panaget@t-online.de>
2022-06-03 10:32:52 +0200

    test structure

---
+++ pages/ui-test-site.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Hallo das ist die UI Test Seite</h1>
+        <TestComponent></TestComponent>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
