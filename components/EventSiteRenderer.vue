70a0726490c60033ab387fdc537ef86abce30b13
patmaeder <padi.maeder@web.de>
2022-06-28 12:05:51 +0200

    load compressed room models

--- components/EventSiteRenderer.vue
+++ components/EventSiteRenderer.vue
@@ -26,7 +26,8 @@
                             :key="tile.id" :src="'/glbModels/' + tile.type + '.glb'" 
                             :position="{x: tile.positionX, y: tile.positionY, z: tile.positionZ}" 
                             @click="focusRoom($event, tile.id)" 
-                            @load="tileLoaded($event, tile.id)" 
+                                @before-load="beforeTileLoad"
+                            @load="tileLoaded($event, tile.id)"
                         />
                     </Group>
                     
@@ -87,6 +88,7 @@ import { Renderer, Camera, Scene, AmbientLight, DirectionalLight, Group, GltfMod
 import * as THREE from "three";
 import { CSS2DRenderer, CSS2DObject } from "three/examples/jsm/renderers/CSS2DRenderer.js";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 import * as Y from "yjs"
 import { WebsocketProvider } from "y-websocket";
 
@@ -181,6 +183,12 @@ const currentEvents = computed(() => {
  * Functions
  * ---------------
  */
+function beforeTileLoad(GLTFLoader) {
+    const loader = new DRACOLoader();
+    loader.setDecoderPath('/node_modules/three/examples/js/libs/draco/');
+    GLTFLoader.setDRACOLoader(loader);
+}
+
 function tileLoaded(gltf, id) {
     gltf.scene.userData.roomID = id;
 
@@ -218,7 +226,7 @@ function characterLoaded(gltf) {
             if (character.value.userData.move.forward != 0 || character.value.userData.move.sideward != 0) {
                 
                 let quaternion = getRotationQuaternion(character.value.userData.move.forward, character.value.userData.move.sideward);
-                characterObject3D.quaternion.rotateTowards(quaternion, 0.04);
+                characterObject3D.quaternion.rotateTowards(quaternion, 4 * delta);
 
                 if (!characterIsIntersecting()) {
                 
@@ -228,7 +236,7 @@ function characterLoaded(gltf) {
 
                     raycaster.set(characterObject3D.position, new THREE.Vector3(1, -1, 0).applyQuaternion(characterObject3D.quaternion));
                     let intersections = raycaster.intersectObjects(tiles.value.group.children);
-                    let tempRoom
+                    let tempRoom;
 
                     if (intersections.length > 0) {
                         characterObject3D.translateX(0.02 * delta * character.value.userData.move.speed);
