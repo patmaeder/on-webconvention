580273bfc291af93b76826ae635c37d90aa0b452
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2022-06-29 23:29:16 +0200

    add env replacement

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+set -Ex
+
+function apply_path {
+
+    echo "Check that we have ENVIROMENT_VAR vars"
+
+    # replace all placeholder occurences with real environment variable
+    find /usr/src/.output \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#YJS_HOST#%YJS_HOST%#g"
+    find /usr/src/.output \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#SFU_HOST#%SFU_HOST%#g"
+}
+
+apply_path
+
 
 npx prisma migrate dev
 npm run ws & node .output/server/index.mjs
+exec "$@"
