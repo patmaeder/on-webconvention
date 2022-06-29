ece2214a18e6928b0f31f2cc67e8042746ecdc5c
Calvin Reibenspieß <calvinreibenspiess@gmail.com>
2022-06-29 23:35:36 +0200

    change env names

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,11 @@ function apply_path {
 
     echo "Check that we have ENVIROMENT_VAR vars"
 
+    echo $YJS_HOST
+
     # replace all placeholder occurences with real environment variable
-    find /usr/src/.output \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#YJS_HOST#%YJS_HOST%#g"
-    find /usr/src/.output \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#SFU_HOST#%SFU_HOST%#g"
+    find /usr/src/.output \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#%YJS_HOST%#$YJS_HOST#g"
+    find /usr/src/.output \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#%SFU_HOST%#$SFU_HOST#g"
 }
 
 apply_path
